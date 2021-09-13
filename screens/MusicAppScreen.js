543c602378a304a2612ceb1605248de3a36f083a
AungMyoMoeKyi <aungmyomoekyi@gmail.com>
2021-09-13 11:08:31 +0630

    second upload

--- screens/MusicAppScreen.js
+++ screens/MusicAppScreen.js
@@ -1,48 +1,64 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity,Image, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, Dimensions } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 import { SimpleLineIcons } from '@expo/vector-icons';
 import Slider from '@react-native-community/slider';
+import { AntDesign } from '@expo/vector-icons';
 
-const{width,height}=Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 
 const MusicAppScreen = props => {
     return (
         <View style={styles.screen}>
             <View style={styles.container}>
                 <View style={styles.imageContainer}>
-                    <Image style={styles.image} source = {require('../assets/OIP.jpg')}/>
+                    <Image style={styles.image} source={require('../assets/235.1.jpg')} />
                 </View>
             </View>
             <View style={styles.textContainer}>
-                <Text style={{fontSize:27,color:'white',fontWeight:'bold'}}>Fancy You</Text>
-                <Text style={{fontSize:20,color:'white',marginTop:10}}>Twice</Text>
+                <Text style={{ fontSize: 27, color: 'white', fontWeight: 'bold' }}>Fancy You</Text>
+                <Text style={{ fontSize: 20, color: 'white', marginTop: 10 }}>Twice</Text>
             </View>
             <View>
-                <Slider 
+                <Slider
                     style={styles.sliderContainer}
                     value={10}
                     minimumValue={0}
-                    maximunValue={100} 
-                    thumbTintColor='red'
-                    minimumTrackTintolor='red'
-                    minimumTrackTintolor='white'
-                    >
+                    maximumValue={100}
+                    thumbTintColor='white'
+                    minimumTrackTintColor='yellow'
+                    maximumTrackTintColor='white'
+                >
                 </Slider>
             </View>
+            <View style={styles.progessLabelContainer}>
+                <Text style={styles.progressLabel}>0:00</Text>
+                <Text style={styles.progressLabel}>3:55</Text>
+            </View>
+            <View style={styles.musicControlContainer}>
+                <TouchableOpacity onPress={()=>{}}>
+                    <AntDesign name="stepbackward" size={24} color="white" style={{marginTop:15}}/>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={()=>{}}>
+                    <AntDesign name="playcircleo" size={50} color="yellow" />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={()=>{}}>
+                    <AntDesign name="stepforward" size={24} color="white" style={{marginTop:15}}/>
+                </TouchableOpacity>
+            </View>
             <View style={styles.buttonContainer}>
-                <TouchableOpacity onPress={()=>{}}>
-                    <Entypo name="heart-outlined" size={24} color="black" />
+                <TouchableOpacity onPress={() => { }}>
+                    <Entypo name="heart-outlined" size={24} color="white" />
                 </TouchableOpacity >
-                <TouchableOpacity onPress={()=>{}}>
-                    <Ionicons name="ios-repeat-sharp" size={28} color="black" style={{}}/>
+                <TouchableOpacity onPress={() => { }}>
+                    <Ionicons name="ios-repeat-sharp" size={28} color="white" style={{}} />
                 </TouchableOpacity>
-                <TouchableOpacity onPress={()=>{}}>
-                    <Entypo name="share-alternative" size={24} color="black" />
+                <TouchableOpacity onPress={() => { }}>
+                    <Entypo name="share-alternative" size={24} color="white" />
                 </TouchableOpacity>
-                <TouchableOpacity onPress={()=>{}}>
-                    <SimpleLineIcons name="options-vertical" size={24} color="black" />
+                <TouchableOpacity onPress={() => { }}>
+                    <SimpleLineIcons name="options-vertical" size={24} color="white" />
                 </TouchableOpacity>
             </View>
         </View>
@@ -51,29 +67,51 @@ const MusicAppScreen = props => {
 
 const styles = StyleSheet.create({
     screen: {
-        backgroundColor: 'gray',
+        backgroundColor: 'black',
         width: '100%',
-        alignItems:'center'
+        alignItems: 'center'
     },
-    imageContainer:{
-        width:200,
-        height:300,
-        marginTop:150,
-        marginBottom:20,
-        alignItems:'center',
+    imageContainer: {
+        width: 200,
+        height: 300,
+        marginTop: 150,
+        marginBottom: 20,
+        alignItems: 'center',
     },
-    image:{
-        width:'100%',
-        height:'100%',
-        borderRadius:10,
+    image: {
+        width: '100%',
+        height: '100%',
+        borderRadius: 10,
     },
-    textContainer:{
-        alignItems:'center',
+    textContainer: {
+        alignItems: 'center',
+    },
+    sliderContainer: {
+        width: 300,
+        height: 40,
+        marginTop: 35,
+        flexDirection: "row",
+    },
+    progessLabelContainer:{
+        width:320,
+        height:40,
+        flexDirection:'row',
+        justifyContent:'space-between',
+    },
+    progressLabel:{
+        color:'white',
+    },
+    musicControlContainer:{
+        width:'60%',
+        flexDirection:'row',
+        justifyContent: 'space-between',
+        marginTop:16,
+        marginBottom:36,
     },
     buttonContainer: {
-        width:width,
-        borderTopColor: 'pink',
-        borderWidth: 2,
+        width: width,
+        borderTopColor: 'yellow',
+        borderWidth: 1,
         padding: 30,
         flexDirection: 'row',
         justifyContent: 'space-between',
